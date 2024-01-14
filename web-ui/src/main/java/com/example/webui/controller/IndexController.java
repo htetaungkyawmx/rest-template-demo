293414243f772afcbd0277a2c077a6f6cf32c83e
uhtetaungkyaw <htetaungkyaw372005@gmail.com>
2024-01-14 22:20:50 +0630

    Add

---
+++ web-ui/src/main/java/com/example/webui/controller/IndexController.java
@@ -0,0 +1,13 @@
+package com.example.webui.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+
+    @GetMapping({"/","/home"})
+    public String index(){
+        return "home";
+    }
+}
